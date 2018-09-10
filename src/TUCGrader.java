bcff46fafaa6dd8865ddd4355d5bcd1a8be015c2
Brian Smith <b.smith@troutmoon.com>
2018-09-10 09:14:24 -0600

    Create TUCGrader app.  Update Jenkins file message.

---
+++ src/TUCGrader.java
@@ -0,0 +1,8 @@
+class TUCGrader {
+
+    public static void main(String[] args) {
+        System.out.println(
+            "Hello Temperature Unit Conversion Grading Application World!"); 
+    }
+
+}
